359b140a674f471f6ee48ffe32e71b50ce39fdfd
Ilya <bkmy43@googlemail.com>
2025-02-21 23:35:36 +0100

    updated config to fix custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/engaged-web/', // Updated to match new repository name
+  base: '/', // Updated for custom domain
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
